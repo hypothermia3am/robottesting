1fbd420d2f3168ac1e76adf9624f7fb7502e24be
cbrown17 <cbrown17@lbpearson.ca>
2023-02-16 18:09:57 -0500

    innit test code2

--- robot.py
+++ robot.py
@@ -5,7 +5,9 @@ class Robot(wp.TimedRobot):
     def robotInit(self):
         self.motor = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless)
         self.turn = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless)
+        self.turnEnconder = self.turn.getAbsoluteEncoder(rev.SparkMaxAbsoluteEncoder.Type.kDutyCycle)
         self.controller = wp.XboxController(0)
+
     
     def teleopPeriodic(self):
        
@@ -17,6 +19,8 @@ class Robot(wp.TimedRobot):
             motorTurnSpeed = 0
         self.motor.set(motorSpeed)
         self.turn.set(motorTurnSpeed)
+    def robotPeriodic(self):
+        print(self.turnEnconder.getPosition())
 
     
 if __name__ == '__main__':
