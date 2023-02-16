9e503a18e0503f3fcc46ca2b9f9bc72e714172ff
cbrown17 <cbrown17@lbpearson.ca>
2023-02-16 18:45:18 -0500

    innit test code4

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import wpilib as wp
 import rev
 import math
+from wpimath.controller import PIDController
 
 class Robot(wp.TimedRobot):
     def robotInit(self):
@@ -8,22 +9,31 @@ class Robot(wp.TimedRobot):
         self.turn = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless)
         self.turnEnconder = self.turn.getAbsoluteEncoder(rev.SparkMaxAbsoluteEncoder.Type.kDutyCycle)
         self.controller = wp.XboxController(0)
-
+        kP = 0.05
+        self.turnPID = PIDController(kP, 0, 0)
     
     def teleopPeriodic(self):
        
         y = self.controller.getRightY()
         x = self.controller.getRightX()
         angle = math.atan2(y, x) + math.pi
-        print(angle)
-        motorSpeed = self.controller.getRawAxis(1)
-        motorTurnSpeed = self.controller.getRawAxis(0)
-        if motorSpeed < 0.05:
-            motorSpeed = 0
-        if motorTurnSpeed < 0.05:
-            motorTurnSpeed = 0
-        self.motor.set(motorSpeed)
-        self.turn.set(motorTurnSpeed)
+        angle += math.pi 
+        angle = angle % (math.pi * 2)
+        currentAngle = self.turnEnconder.getPosition()
+        print("angle", angle, "currentAngle", currentAngle)
+        motorturnSpeed = self.turnPID.calculate(currentAngle, angle)
+        self.turn.set(motorturnSpeed)
+
+        # print(angle)
+        # motorSpeed = self.controller.getRawAxis(1)
+        # motorTurnSpeed = self.controller.getRawAxis(0)
+        # if motorSpeed < 0.05:
+        #     motorSpeed = 0
+        # if motorTurnSpeed < 0.05:
+        #     motorTurnSpeed = 0
+        # self.motor.set(motorSpeed)
+        # self.turn.set(motorTurnSpeed)
+
     def robotPeriodic(self):
         pass
     
